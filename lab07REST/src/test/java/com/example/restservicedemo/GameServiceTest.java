95a38d644a8534362ad58f3819c1a3e1196f0eb1
Redellex <masterpaw5679966@gmail.com>
2016-06-09 00:57:45 +0200

    Reworked, refactored REST tests.

--- lab07REST/src/test/java/com/example/restservicedemo/GameServiceTest.java
+++ lab07REST/src/test/java/com/example/restservicedemo/GameServiceTest.java
@@ -26,17 +26,26 @@ public class GameServiceTest {
 	private static final String GAME_NAME_2 = "World of Warcraft";
 	private static final String GAME_GENRE_2 = "MMORPG";
 	
+	//Funkcja skracajaca kod. Nie chce by testy byly od siebie zalezne...
+	private Game addGameHelper(long id, String gameName, String genre)
+	{
+		Game game = new Game(id, gameName, genre);
+		
+		given().
+	       contentType(MediaType.APPLICATION_JSON).
+	       body(game).
+	    when().	     
+	    post("/game/").then().assertThat().statusCode(201);
+		
+		return game;
+	}
+	
 	@BeforeClass
     public static void setUp(){
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = 8080;
 		RestAssured.basePath = "/restservicedemo/api";   	
     }
-	
-	@After
-	public void cleanUp(){
-		delete("/game/").then().assertThat().statusCode(200);
-	}
 	
 	@Test
 	public void addGames(){		
@@ -51,64 +60,53 @@ public class GameServiceTest {
 		Game rGame = get("/game/1").as(Game.class);
 		
 		assertThat(GAME_NAME, equalToIgnoringCase(rGame.getName()));
+	}
+	
+	@Test
+	public void readGames()
+	{
+		addGameHelper(1l, GAME_NAME, GAME_GENRE);
+		addGameHelper(2l, GAME_NAME, GAME_GENRE_2);
+		addGameHelper(3l, GAME_NAME_2, GAME_GENRE_2);
+		addGameHelper(4l, GAME_NAME_2, GAME_GENRE);
 		
+	    RestAssured.when().get("/game/").then().assertThat().statusCode(200);
+	    
+	    GameResponse response = get("/game/").as(GameResponse.class);
+	    List<Game> games = response.getGame();
+	    assertEquals(4, games.size());
 	}
 	
 	@Test
 	public void deleteGames(){
-		Game game = new Game(1L, GAME_NAME, GAME_GENRE);
-		Game game1 = new Game(2L, GAME_NAME, GAME_GENRE);
+		final int beforeDeleteCount = 3;
+		final int afterDeleteCount = 2;
+	
+		Game game;
+		game = addGameHelper(1l, GAME_NAME, GAME_GENRE);
+		addGameHelper(2L, GAME_NAME, GAME_GENRE_2);
+		addGameHelper(3L, GAME_NAME_2, GAME_GENRE);
 		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game).
-	    when().
-	    post("/game/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game1).
-	    when().
-	    post("/game/").then().assertThat().statusCode(201);
+		GameResponse response = get("/game/").as(GameResponse.class);
+	    List<Game> games = response.getGame();
+	    assertEquals(beforeDeleteCount, games.size());
 		
 		given().
 			contentType(MediaType.APPLICATION_JSON).body(game).
 		when().
 		delete("/game/1").then().assertThat().statusCode(200);
+		
+		response = get("/game/").as(GameResponse.class);
+		games = response.getGame();
+		assertEquals(afterDeleteCount, games.size());
 	}
-	
 	
 	@Test
 	public void updateGames(){
-		Game game = new Game (1l, GAME_NAME, GAME_GENRE);
-		Game game1 = new Game (2l, GAME_NAME, GAME_GENRE_2);
-		Game game2 = new Game (3l, GAME_NAME_2, GAME_GENRE_2);
-		Game game3 = new Game (4l, GAME_NAME_2, GAME_GENRE);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game).
-	    when().	     
-	    post("/game/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game1).
-	    when().	     
-	    post("/game/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game2).
-	    when().	     
-	    post("/game/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game3).
-	    when().	     
-	    post("/game/").then().assertThat().statusCode(201);
-		
+		addGameHelper(1l, GAME_NAME, GAME_GENRE);
+		addGameHelper(2l, GAME_NAME, GAME_GENRE_2);
+		addGameHelper(3l, GAME_NAME_2, GAME_GENRE_2);
+		addGameHelper(4l, GAME_NAME_2, GAME_GENRE);
 		// Update
 		
 		given().
@@ -117,44 +115,13 @@ public class GameServiceTest {
 			pathParam("genre", "FPSe").
 		when().	
 			put("/game/{gameId}/{name}/{genre}").then().assertThat().statusCode(200);
+		
+			Game rGame = get("/game/1").as(Game.class);
+			assertEquals("super", rGame.getName());
 	}
 	
-	@Test
-	public void readGames()
-	{
-		Game game = new Game (1l, GAME_NAME, GAME_GENRE);
-		Game game1 = new Game (2l, GAME_NAME, GAME_GENRE_2);
-		Game game2 = new Game (3l, GAME_NAME_2, GAME_GENRE_2);
-		Game game3 = new Game (4l, GAME_NAME_2, GAME_GENRE);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game).
-	    when().	     
-	    post("/game/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game1).
-	    when().	     
-	    post("/game/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game2).
-	    when().	     
-	    post("/game/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(game3).
-	    when().	     
-	    post("/game/").then().assertThat().statusCode(201);	
-		
-	    RestAssured.when().get("/game/").then().assertThat().statusCode(200);
-	    
-	    GameResponse response = get("/game/").as(GameResponse.class);
-	    List<Game> games = response.getGame();
-	    assertEquals(4, games.size());
+	@After
+	public void cleanUp(){
+		delete("/game/").then().assertThat().statusCode(200);
 	}
 }
--- lab07REST/src/test/java/com/example/restservicedemo/ProducentServiceTest.java
+++ lab07REST/src/test/java/com/example/restservicedemo/ProducentServiceTest.java
@@ -22,7 +22,7 @@ import com.example.restservicedemo.domain.ProducentResponse;
 import com.jayway.restassured.RestAssured;
 
 public class ProducentServiceTest {
-	
+
 	private static final String PRODUCENT_NAME = "Blizzard";
 	private static final long GAME_ID = 10L;
 	private static final String PRODUCENT_NAME_2 = "World of Warcraft";
@@ -30,29 +30,41 @@ public class ProducentServiceTest {
 	private static final String GAME_NAME = "DOOM";
 	private static final String GAME_GENRE = "FPS";
 	
-	@BeforeClass
-    public static void setUp(){
-		RestAssured.baseURI = "http://localhost";
-		RestAssured.port = 8080;
-		RestAssured.basePath = "/restservicedemo/api"; 
-		Game game = new Game(10L, GAME_NAME, GAME_GENRE);
+	private Producent addProducentHelper(long id, String name, long gameId)
+	{
+		Producent producent = new Producent(id, name, gameId);
+		
+		given().
+	       contentType(MediaType.APPLICATION_JSON).
+	       body(producent).
+	    when().	     
+	    post("/producent/").then().assertThat().statusCode(201);
+		
+		return producent;
+	}
+	
+	private static void addGameHelper(long id, String gameName, String genre)
+	{
+		Game game = new Game(id, gameName, genre);
 		
 		given().
 	       contentType(MediaType.APPLICATION_JSON).
 	       body(game).
 	    when().	     
 	    post("/game/").then().assertThat().statusCode(201);
+	}
+	
+	@BeforeClass
+    public static void setUp(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8080;
+		RestAssured.basePath = "/restservicedemo/api"; 
     }
 	
-	@After
-	public void cleanUp(){
-		delete("/producent/").then().assertThat().statusCode(200);
-		delete("/game/").then().assertThat().statusCode(200);
-	}
-
-	
 	@Test
-	public void addProducent(){		
+	public void addProducent(){	
+		addGameHelper(GAME_ID, GAME_NAME, GAME_GENRE);
+		addGameHelper(GAME_ID_2, GAME_NAME, GAME_GENRE);
 		Producent producent = new Producent(1L, PRODUCENT_NAME, GAME_ID);
 		
 		given().
@@ -62,84 +74,76 @@ public class ProducentServiceTest {
 	    post("/producent/").then().assertThat().statusCode(201);
 				
 		Producent rProducent = get("/producent/1").as(Producent.class);
+		
 		assertThat(PRODUCENT_NAME, equalToIgnoringCase(rProducent.getName()));
 		assertEquals(GAME_ID, rProducent.getIdGame());
 	}
-	
-	@Test
-	public void deleteProducent(){
-		Producent producent = new Producent(1L, PRODUCENT_NAME, GAME_ID);
-		Producent producent2 = new Producent(2L, PRODUCENT_NAME, GAME_ID);
 		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(producent).
-	    when().	     
-	    post("/producent/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(producent2).
-	    when().	     
-	    post("/producent/").then().assertThat().statusCode(201);
-		
-		given().
-		contentType(MediaType.APPLICATION_JSON).body(producent).
-		when().
-		delete("/producent/1").then().assertThat().statusCode(200);
-	}
-	
-	@Test
-	public void updateProducent(){
-		Producent producent = new Producent(1L, PRODUCENT_NAME_2, GAME_ID_2);
-		Producent producent2 = new Producent(2L, PRODUCENT_NAME, GAME_ID);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(producent).
-	    when().	     
-	    post("/producent/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(producent2).
-	    when().	     
-	    post("/producent/").then().assertThat().statusCode(201);
-		
-		// Update
-		given().
-			pathParam("ProducentId", "1").
-			pathParam("ProducentName", "Capcom").
-			pathParam("ProducentGameId", "1").
-		when().	
-			put("/Producent/{ProducentId}/{ProducentName}/{ProducentGameId}").then().assertThat().statusCode(200);
-	}
-	
 	@Test
 	public void readProducent()
 	{
-		Producent producent = new Producent(1L, PRODUCENT_NAME_2, GAME_ID_2);
-		Producent producent2 = new Producent(2L, PRODUCENT_NAME, GAME_ID);
+		final int EXPECTED_SIZE = 2;
 		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(producent).
-	    when().	     
-	    post("/producent/").then().assertThat().statusCode(201);
-		
-		given().
-	       contentType(MediaType.APPLICATION_JSON).
-	       body(producent2).
-	    when().	     
-	    post("/producent/").then().assertThat().statusCode(201);
-			
+		addGameHelper(GAME_ID, GAME_NAME, GAME_GENRE);
+		addGameHelper(GAME_ID_2, GAME_NAME, GAME_GENRE);
+		addProducentHelper(1L, PRODUCENT_NAME_2, GAME_ID_2);
+		addProducentHelper(2L, PRODUCENT_NAME, GAME_ID);
 		
 	    RestAssured.when().get("/producent/").then().assertThat().statusCode(200);
 	    
 	    ProducentResponse response = get("/producent/").as(ProducentResponse.class);
 	    List<Producent> producents = response.getProducent();
-	    assertEquals(2, producents.size());
+	    assertEquals(EXPECTED_SIZE, producents.size());
 	}
 	
+	@Test
+	public void deleteProducent(){
+		final int beforeDeleteCount = 3;
+		final int afterDeleteCount = 2;
+		
+		addGameHelper(GAME_ID, GAME_NAME, GAME_GENRE);
+		addGameHelper(GAME_ID_2, GAME_NAME, GAME_GENRE);
+		Producent producent = addProducentHelper(1L, PRODUCENT_NAME, GAME_ID);
+		addProducentHelper(2L, PRODUCENT_NAME, GAME_ID);
+		addProducentHelper(3L, PRODUCENT_NAME_2, GAME_ID);
+		
+		ProducentResponse response = get("/producent/").as(ProducentResponse.class);
+	    List<Producent> producents = response.getProducent();
+	    assertEquals(beforeDeleteCount, producents.size());
+		
+		given().
+		contentType(MediaType.APPLICATION_JSON).body(producent).
+		when().
+		delete("/producent/1").then().assertThat().statusCode(200);
+		
+		response = get("/producent/").as(ProducentResponse.class);
+		producents = response.getProducent();
+		assertEquals(afterDeleteCount, producents.size());
+	}
 	
+	@Test
+	public void updateProducent(){
+		addGameHelper(GAME_ID, GAME_NAME, GAME_GENRE);
+		addGameHelper(GAME_ID_2, GAME_NAME, GAME_GENRE);
+		addProducentHelper(1L, PRODUCENT_NAME, GAME_ID);
+		
+		// Update
+		given().
+			pathParam("ProducentId", "1").
+			pathParam("ProducentName", "Capcom").
+			pathParam("ProducentGameId", GAME_ID_2).
+		when().	
+			put("/producent/{ProducentId}/{ProducentName}/{ProducentGameId}").then().assertThat().statusCode(200);
+		
+		Producent rProducent = get("/producent/1").as(Producent.class);
+		
+		assertThat("Capcom", equalToIgnoringCase(rProducent.getName()));
+		assertEquals(GAME_ID_2, rProducent.getIdGame());
+	}
+	
+	@After
+	public void cleanUp(){
+		delete("/producent/").then().assertThat().statusCode(200);
+		delete("/game/").then().assertThat().statusCode(200);
+	}	
 }
